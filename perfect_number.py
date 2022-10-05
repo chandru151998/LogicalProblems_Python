9a66c3123f72afbb7278d3d03ec73f1676c6cc6f
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-10-05 12:21:58 +0530

    perfect number

---
+++ perfect_number.py
@@ -0,0 +1,17 @@
+def perfect_number(n):
+    '''
+    perfect_number
+    :return:
+    '''
+    sum1 = 0
+    for i in range(1, n):
+        if (n % i == 0):
+            sum1 = sum1 + i
+    if (sum1 == n):
+        print("The number is a Perfect number")
+    else:
+        print("The number is not a Perfect number")
+
+if __name__ == '__main__':
+    n = int(input("Enter any number: "))
+    perfect_number(n)
